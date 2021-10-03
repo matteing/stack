294482154a8ea23a1cdfefb48f68a98c51685bec
Sergio Mattei <sergio@getmakerlog.com>
2021-10-03 00:14:56 -0400

    Fix NextJS starter

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,20 +1,19 @@
 import { LayoutTree } from "@moxy/next-layout";
 import { Provider } from "next-auth/client";
-import Shell from "components/Shell";
 import { ClientTokenRefresher } from "lib/client";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
 	return (
 		<Provider session={pageProps.session}>
 			<ClientTokenRefresher />
 			<LayoutTree
 				Component={Component}
 				pageProps={pageProps}
-				defaultLayout={<Shell />}
+				/* defaultLayout={<Shell />} */
 			/>
 		</Provider>
 	);
 }
 
-export default MyApp;
+export default App;
--- app/queries/example.js
+++ app/queries/example.js
@@ -1,12 +1,8 @@
 import client from "lib/client";
 import { useQuery } from "react-query";
 
-export const METRICS_QUERIES = {
-	getMetrics: "metrics.getMetrics",
-};
-
 export function useMetrics() {
-	return useQuery([METRICS_QUERIES.getMetrics], async () => {
+	return useQuery(["metrics.getMetrics"], async () => {
 		const { data } = await client.get("/metrics");
 		return data;
 	});
