315bdd5b60ae36d1a299b6ae6721477f552a5736
Sergio Mattei <sergiomattei@outlook.com>
2021-10-04 18:59:53 -0400

    Fix React-Query configuration

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,17 +1,25 @@
+import React from "react";
 import { LayoutTree } from "@moxy/next-layout";
 import { Provider } from "next-auth/client";
 import { ClientTokenRefresher } from "lib/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
+	const [queryClient] = React.useState(() => new QueryClient());
 	return (
 		<Provider session={pageProps.session}>
 			<ClientTokenRefresher />
-			<LayoutTree
-				Component={Component}
-				pageProps={pageProps}
-				/* defaultLayout={<Shell />} */
-			/>
+			<QueryClientProvider client={queryClient}>
+				<Hydrate state={pageProps.dehydratedState}>
+					<LayoutTree
+						Component={Component}
+						pageProps={pageProps}
+						/* defaultLayout={<Layout />} */
+					/>
+				</Hydrate>
+			</QueryClientProvider>
 		</Provider>
 	);
 }
